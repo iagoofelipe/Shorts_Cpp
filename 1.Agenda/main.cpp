a5489abd8acd2d3132d956b957258237b8f4c514
Iago Carvalho <iagoo.feipe@gmail.com>
2024-08-15 17:11:49 -0300

    add 1.Agenda

---
+++ 1.Agenda/main.cpp
@@ -0,0 +1,130 @@
+/* 
+
+ Agenda de Eventos
+
+ Componentes:
+    - <class> Menu
+
+*/
+
+// menu
+// 
+
+#include <iostream>
+#include <limits>
+#include <time.h>
+
+#define PRINT_SEP() { std::cout << "\n--------------------------\n"; }
+
+// dados de um evento
+struct Event
+{
+    std::string title;
+    time_t date;
+    std::string description;
+};
+
+// Classe principal para o menu, utilizado para receber e tratar as informações enviadas pelo usuário
+class Menu
+{
+
+private:
+    void displayOptions()
+    {
+        int op;
+        int num_opts = 2;
+        int ops[num_opts] = {1, 2};
+        bool keepRunning = true;
+
+        while(keepRunning)
+        {
+            PRINT_SEP();
+            std::cout
+                << "Bem-Vindo a sua Agenda!\n" << std::endl
+                << "1. Novo evento" << std::endl
+                << "2. Eventos disponiveis" << std::endl
+                << "3. Sair\n" << std::endl
+                // << "\nOpcao escolhida: "
+            ;
+
+            // getUserInput<int>(op, "Opcao escolhida", "Apenas numeros sao aceitos!");
+            std::cout << "Opcao escolhida: ";
+            std::cin >> op;
+            PRINT_SEP();
+            
+            std::string title;
+            switch (op)
+            {
+            case 1:
+                // newEvent();
+                std::cout << "Titulo do evento: ";
+                std::getline(std::cin, title);
+                break;
+            
+            case 2:
+                
+                break;
+            
+            case 3:
+                keepRunning = false;
+                break;
+
+            default:
+                std::cout << "Escolha uma opcao dentre as disponiveis!\n";
+                break;
+            }
+        }
+    }
+
+    template <typename T> void getUserInput(T &in, std::string msg_inp, std::string msg_error)
+    {
+        while(true)
+        {
+            std::cout << msg_inp + ": ";
+            std::cin >> in;
+
+            if (std::cin)
+                break;
+
+            
+
+            // caso TypeError
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            
+            std::cout << msg_error + "\n\n";
+        }
+    }
+
+public:
+    void run()
+    {
+        displayOptions();
+    }
+
+    void newEvent()
+    {
+        std::string title;
+        // Event evt;
+
+        std::cout << "Titulo: ";
+        std::getline(std::cin, title);
+
+        std::cout << title << std::endl;
+
+    }
+};
+
+int main()
+{
+    Menu m;
+    m.run();
+
+    // std::string title;
+    // // Event evt;
+
+    // std::cout << "Titulo: ";
+    // std::getline(std::cin, title);
+
+    // std::cout << title << std::endl;
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Shorts_Cpp
 Projetos curtos em C++
+
+# Projetos
+ - [Agenda](#agenda)
+
+## Agenda
+Aplicativo de agenda simples, onde será possível adicionar, visualizar, editar e excluir eventos. Cada evento terá informações como título, data, hora e descrição.
+
+Pasta com código-fonte, [1.Agenda](./1.Agenda/).
